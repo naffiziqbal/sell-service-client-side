3dae677f2a305d067547aadc0f6f4ba106af4a2f
naffiziqbal <f.nafiziqbal@gmail.com>
2022-12-21 01:16:40 +0600

    Adding Swipper Js in Banner

--- src/Components/Home/Home/Section/Banner.jsx
+++ src/Components/Home/Home/Section/Banner.jsx
@@ -1,35 +1,100 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
 const Banner = () => {
   return (
     <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          {" "}
+          <div id="item1" className="carousel-item w-full">
+            <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
+              <p>Machine That Every One Desire</p>
+            </div>
+            <img
+              src="https://i.ibb.co/7bfB3Yv/harley-davidson-1-HZc-Jjdtc9g-unsplash.jpg"
+              alt=""
+              className="w-full opacity-20"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div id="item2" className="relative carousel-item w-full">
+            <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
+              <p>Machine That Every One Loves</p>
+            </div>
+            <img
+              src="https://i.ibb.co/WvLHQ8t/harley-davidson-ee-TJKC-wz34-unsplash.jpg"
+              alt=""
+              className="w-full opacity-20"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <div id="item3" className="relative carousel-item w-full">
+            <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
+              <p>Machine That Every One Urges</p>
+            </div>
+            <img
+              src="https://i.ibb.co/NnKhrjJ/marcus-ganahl-olwehv-Chxfg-unsplash.jpg"
+              alt=""
+              className="w-full opacity-20"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
       <div className=" relative carousel w-full">
-        <div id="item1" className="carousel-item w-full">
+        {/* <div id="item1" className="carousel-item w-full">
         <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
             <p>
               Machine That Every One Desire
             </p>
           </div>
           <img src="https://i.ibb.co/7bfB3Yv/harley-davidson-1-HZc-Jjdtc9g-unsplash.jpg" alt="" className="w-full opacity-20" />
-        </div>
-        <div id="item2" className="relative carousel-item w-full">
+        </div> */}
+        {/* <div id="item2" className="relative carousel-item w-full">
           <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
-            <p>
-              Machine That Every One Loves
-            </p>
+            <p>Machine That Every One Loves</p>
           </div>
-          <img src="https://i.ibb.co/WvLHQ8t/harley-davidson-ee-TJKC-wz34-unsplash.jpg" alt="" className="w-full opacity-20" />
-        </div>
-        <div id="item3" className="relative carousel-item w-full">
-        <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
-            <p>
-              Machine That Every One Urges
-            </p>
+          <img
+            src="https://i.ibb.co/WvLHQ8t/harley-davidson-ee-TJKC-wz34-unsplash.jpg"
+            alt=""
+            className="w-full opacity-20"
+          />
+        </div> */}
+        {/* <div id="item3" className="relative carousel-item w-full">
+          <div className="absolute text-3xl font-bold text-yellow-500 top-2/4">
+            <p>Machine That Every One Urges</p>
           </div>
-          <img src="https://i.ibb.co/NnKhrjJ/marcus-ganahl-olwehv-Chxfg-unsplash.jpg" alt="" className="w-full opacity-20" />
-        </div>
+          <img
+            src="https://i.ibb.co/NnKhrjJ/marcus-ganahl-olwehv-Chxfg-unsplash.jpg"
+            alt=""
+            className="w-full opacity-20"
+          />
+        </div> */}
       </div>
-      <div className="flex justify-center w-full py-2 gap-2">
+      
+      {/* <div className="flex justify-center w-full py-2 gap-2">
         <a href="#item1" className="btn btn-xs">
           1
         </a>
@@ -39,7 +104,7 @@ const Banner = () => {
         <a href="#item3" className="btn btn-xs">
           3
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
